247365d833faee8c1d22291780a5669c4482ebbf
gachudon <doneson7@gmail.com>
2025-01-15 16:03:17 +0900

    react-icons added

--- my-blog/src/components/layout/Aside.jsx
+++ my-blog/src/components/layout/Aside.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import {FaGithub} from "react-icons/fa";
+import {IoMail} from "react-icons/io5";
 
 import './css/layout.css';
 import { StyledAside } from "./style/StyledLayout";
@@ -7,6 +9,10 @@ function Aside(){
     return(
         <StyledAside>
             <h2>허승돈</h2>
+            <ul>
+                <li><a href="mailto:doneson7@gmail.com"><IoMail/>Email</a></li>
+                <li><a href="https://github.com/Katsudon8991/katsudon8991.github.io"><FaGithub/>Github</a></li>
+            </ul>
         </StyledAside>
     );
 }
